15b5046510f6ed12263f8d94edd74457a7ee8fc3
Roqaia Hassan <roqaiahassanahmed@gmail.com>
2024-10-24 22:10:34 +0300

    implement iterative binary search

--- searching_methods.cpp
+++ searching_methods.cpp
@@ -6,6 +6,27 @@ using namespace std;
 // implement recursive sequential search
 
 // implement binary search 
+int arr[5];
+
+int binarySearch(int arr[], int low, int high, int target)
+{
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        
+        if (arr[mid] == target)
+            return mid;
+
+       
+        if (arr[mid] < target)
+            low = mid + 1;
+
+        else
+            high = mid - 1;
+    }
+
+    return -1;
+}
 
 // implement recursive binary search 
 
