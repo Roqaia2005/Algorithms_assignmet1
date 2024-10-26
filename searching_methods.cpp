17bfb569e2d3956a0ac01a4c45b6524509267dd2
Fatimah3844 <148279022+Fatimah3844@users.noreply.github.com>
2024-10-26 19:55:22 +0300

    Update searching_methods.cpp

--- searching_methods.cpp
+++ searching_methods.cpp
@@ -3,8 +3,27 @@
 using namespace std;
 
 // implement sequential search
+int sequentialSearch(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i; // key found at index i
+        }
+    }
+    return -1; // key not found
+}
+
 
 // implement recursive sequential search
+int recursiveSequentialSearch(int arr[], int size, int index, int target) {
+    if (index >= size) {
+        return -1; // key not found
+    }
+    if (arr[index] == target) {
+        return index; // key found at index
+    }
+    return recursiveSequentialSearch(arr, size, index + 1, target); 
+}
+
 
 // implement binary search
 int arr[5];
@@ -51,15 +70,24 @@ int binarySearch(int arr[], int low, int high, int target)
 
 
 // test your function in main
-
-int main(){
-
-
-    int arr[5]={1,2,3,4,5};
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
     int key;
-     cout<< "enter element you search for "<<endl;
-     cin>>key;
-    cout<<binarySearch(arr, 0, 4, key)<<endl;
-    cout<<RBsearch(arr,0,4,key)<<endl;
+
+    cout << "Enter element you search for: ";
+    cin >> key;
+
+    // Test Sequential Search
+    cout << "Sequential Search Result: " << sequentialSearch(arr, 5, key) << endl;
+
+    // Test Recursive Sequential Search
+    cout << "Recursive Sequential Search Result: " << recursiveSequentialSearch(arr, 5, 0, key) << endl;
+
+    // Test Binary Search
+    cout << "Binary Search Result: " << binarySearch(arr, 0, 4, key) << endl;
+
+    // Test Recursive Binary Search
+    cout << "Recursive Binary Search Result: " << RBsearch(arr, 0, 4, key) << endl;
+
     return 0;
 }
