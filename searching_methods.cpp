9e5a321c88f3377d7363313aa307dda46f0b47ab
Fatimah3844 <148279022+Fatimah3844@users.noreply.github.com>
2024-10-26 17:42:47 +0300

    Update searching_methods.cpp

--- searching_methods.cpp
+++ searching_methods.cpp
@@ -2,11 +2,28 @@
 #include <vector>
 using namespace std;
 
-// implement sequential search 
+// implement sequential search
 
-// implement recursive sequential search
+// implement recursive sequential search---------->//Done
 
-// implement binary search 
+  int RBsearch(int arr[], int low,int high,int key){
+    if (low > high) {
+        return -1; // Not found
+    }
+      else
+      {
+          int mid =(low+high)/2;
+          if(key==arr[mid])
+              return mid;
+          if(key<arr[mid])
+              return RBsearch(arr,low,mid-1,key);
+          else
+              return RBsearch(arr,mid+1,high,key);
+      }
+
+  }
+
+// implement binary search
 int arr[5];
 
 
@@ -16,11 +33,11 @@ int binarySearch(int arr[], int low, int high, int target)
     while (low <= high) {
         int mid = low + (high - low) / 2;
 
-        
+
         if (arr[mid] == target)
             return mid;
 
-       
+
         if (arr[mid] < target)
             low = mid + 1;
 
@@ -31,16 +48,18 @@ int binarySearch(int arr[], int low, int high, int target)
     return -1;
 }
 
-// implement recursive binary search 
+// implement recursive binary search
 
-// test your function in main 
+// test your function in main
 
 int main(){
-   
+
 
     int arr[5]={1,2,3,4,5};
-
-    binarySearch(arr, 0, 4, 5);
-
+    int key;
+     cout<< "enter element you search for "<<endl;
+     cin>>key;
+    cout<<binarySearch(arr, 0, 4, key)<<endl;
+    cout<<RBsearch(arr,0,4,key)<<endl;
     return 0;
 }
